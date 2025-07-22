24ab2be5c56c49ba0be7dd8a2dd5a9c8b21ed0b0
sjndowair <wodnjsdl1125@naver.com>
2025-07-22 22:39:26 +0900

    delete: 사용하지 않는 기존 버전 컴포넌트 삭제

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,6 +1,16 @@
+
 "use client"
+
 import { ThemeProvider as NextThemesProvider } from "next-themes"
-import type { ThemeProviderProps } from "next-themes/dist/types"
+
+interface ThemeProviderProps {
+  children: React.ReactNode
+  attribute?: any  // 유연한 타입
+  defaultTheme?: string
+  enableSystem?: boolean
+  disableTransitionOnChange?: boolean
+  [key: string]: any  // 추가 props 허용
+}
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
